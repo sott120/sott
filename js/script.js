4f0a4d1fcc9db891d60b762d383f886df1fbc694
qirtudgus <qirtudgus@naver.com>
2022-02-27 05:06:51 +0900

    arrow 버튼 기능 구현

--- js/script.js
+++ js/script.js
@@ -10,14 +10,31 @@ function download(){
 })
 };
 
-function viewArrow(){
-    console.log("하하");
-    let arrow2 = document.getElementsByClassName(".arrow");
-    let test = document.getElementById("#test");
- console.log(test);
- test.style.display = "none";
+let arrow4 = document.querySelectorAll('.choice');
 
-arrow2.style.display = "block";
+let arrow3 = document.querySelectorAll('.arrow');
 
+function viewArrow(event){
+    console.log("하하")
+    console.log(event)
 
-}
+    let arrowArr = Object.values(arrow3); //https://bobbyhadz.com/blog/javascript-typeerror-object-map-is-not-a-function
+
+    arrowArr.map((i,index) => { // arrow를 전부 display none 처리
+        i.style.display = "none";
+    });
+
+    event.target.childNodes[1].style.display = 'block'; // event가 일어난 두번째 자식요소(arrow)의 스타일을 변경
+};
+
+function clickArrow(event){
+    event.stopPropagation() //https://gcomx.blogspot.com/2018/02/blog-post_20.html
+    console.log("화살표")
+
+};
+document.querySelectorAll('.choice')[0].addEventListener('click',viewArrow);
+document.querySelectorAll('.choice')[1].addEventListener('click',viewArrow);
+document.querySelectorAll('.choice')[2].addEventListener('click',viewArrow);
+document.getElementsByClassName('arrow')[0].addEventListener('click',clickArrow)
+document.getElementsByClassName('arrow')[1].addEventListener('click',clickArrow)
+document.getElementsByClassName('arrow')[2].addEventListener('click',clickArrow)

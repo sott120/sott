792204a11a11cee77b6ed4ec33513d50098af382
qirtudgus <qirtudgus@naver.com>
2022-03-04 08:15:18 +0900

    화살표 같이 비활성화, border 값 통일

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ let arrow4 = document.querySelectorAll('.choice');
 
 let arrow3 = document.querySelectorAll('.arrow');
 
+<<<<<<< Updated upstream
 function viewArrow(event){
     console.log("하하")
     console.log(event)
@@ -26,9 +27,13 @@ function viewArrow(event){
 
     event.target.childNodes[1].style.display = 'block'; // event가 일어난 두번째 자식요소(arrow)의 스타일을 변경
 };
+=======
+>>>>>>> Stashed changes
 
+// 없으면 choice_click이 arrow에도 전파가 돼요
 function clickArrow(event){
     event.stopPropagation() //https://gcomx.blogspot.com/2018/02/blog-post_20.html
+<<<<<<< Updated upstream
     console.log("화살표")
 
 };
@@ -38,3 +43,30 @@ document.querySelectorAll('.choice')[2].addEventListener('click',viewArrow);
 document.getElementsByClassName('arrow')[0].addEventListener('click',clickArrow)
 document.getElementsByClassName('arrow')[1].addEventListener('click',clickArrow)
 document.getElementsByClassName('arrow')[2].addEventListener('click',clickArrow)
+=======
+};
+
+
+function cercleClick(event){
+// console.log(event.target.classLi
+    if (event.target.classList[1] === "choice_click") {
+        event.target.classList.remove("choice_click");
+        event.target.childNodes[2].classList.remove("arrow_active");
+    } else { 
+        for (let i = 0; i < arrow4.length; i++) {
+            arrow4[i].classList.remove("choice_click");
+            arrow3[i].classList.remove("arrow_active");
+        }
+        event.target.classList.add("choice_click");
+        event.target.childNodes[2].classList.add("arrow_active");
+    }
+}
+
+function init() {
+    for (let i = 0; i < arrow4.length; i++) {
+      arrow4[i].addEventListener("click", cercleClick);
+      arrow3[i].addEventListener("click",clickArrow);
+    }
+  }
+  init();
+>>>>>>> Stashed changes
